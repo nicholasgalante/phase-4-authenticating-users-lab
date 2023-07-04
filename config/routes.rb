f30df6cc98f260b88668eb28dcbb07e41dab08d1
Nicholas Galante <nicholasgalante94@gmail.com>
2023-07-03 22:03:40 -0400

    commit tree 9625de0cbfa3743a4ce1e201c7d033b641c48687

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
